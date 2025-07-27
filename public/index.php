9404a85c1de8a82ccbb2ebb1581335badcf790a7
Chiheb <chiheb.hadjamor@datapowa.fr>
2025-07-27 14:07:42 +0200

    Rollback Deploy temporary Symfony bypassing logging system and Temporarly add throw Exception to AccountController::createAccountAjax

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
-// TEMPORARY DEBUGGING - Force raw PHP error logging
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
-ini_set('error_log', dirname(__DIR__).'/var/log/php_errors.log');
-error_log("--- PHP execution started in index.php ---");
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
